ad4b535739c7060021b5f3726ef66ec529272db4
fschildt <florian.schildt@proton.me>
2022-12-08 23:08:24 +0100

    first commit

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AnsiWordCounterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.progressUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.filenameLabel = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.cancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.progressBar.Location = new System.Drawing.Point(32, 107);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(360, 30);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Visible = false;
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.filenameTextBox.Location = new System.Drawing.Point(32, 47);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.Size = new System.Drawing.Size(281, 20);
+            this.filenameTextBox.TabIndex = 1;
+            this.filenameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.filenameTextBox_KeyDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.browseButton.Location = new System.Drawing.Point(317, 46);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.startButton.Location = new System.Drawing.Point(32, 73);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // progressUpdateTimer
+            // 
+            this.progressUpdateTimer.Tick += new System.EventHandler(this.progressUpdateTimer_Tick);
+            // 
+            // filenameLabel
+            // 
+            this.filenameLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.filenameLabel.AutoSize = true;
+            this.filenameLabel.Location = new System.Drawing.Point(29, 31);
+            this.filenameLabel.Name = "filenameLabel";
+            this.filenameLabel.Size = new System.Drawing.Size(57, 13);
+            this.filenameLabel.TabIndex = 4;
+            this.filenameLabel.Text = "File Name:";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.Location = new System.Drawing.Point(32, 139);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(360, 220);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.Visible = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(113, 73);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 398);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.filenameLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filenameTextBox);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AnsiWordCounter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer progressUpdateTimer;
+        private System.Windows.Forms.Label filenameLabel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AnsiWordCounterApp
+{
+    public partial class Form1 : Form
+    {
+        private AnsiWordCounter mAnsiWordCounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mAnsiWordCounter = new AnsiWordCounter();
+        }
+        
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            this.filenameTextBox.Text = ofd.FileName;
+            this.filenameTextBox.SelectionStart = this.filenameTextBox.Text.Length;
+            this.filenameTextBox.SelectionLength = 0;
+            this.filenameTextBox.Focus();
+        }
+
+        private void filenameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                StartCounting();
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartCounting();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            CancelCounting();
+        }
+
+        private void progressUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            int progressPercentage = mAnsiWordCounter.GetProgressPercentage();
+            if (progressPercentage == -1)
+            {
+                string errorMessage = mAnsiWordCounter.GetErrorMessage();
+                CancelCounting();
+
+                SystemSounds.Asterisk.Play();
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK);
+            }
+            else if (progressPercentage == 100)
+            {
+                List<CountedWord> countedWords = mAnsiWordCounter.GetCountedWords();
+                mAnsiWordCounter.Reset();
+
+                ShowCountedWords(countedWords);
+                this.progressBar.Hide();
+                this.progressUpdateTimer.Stop();
+            }
+            else
+            {
+                int increment = progressPercentage - this.progressBar.Value;
+                if (increment > 0)
+                {
+                    this.progressBar.Increment(increment);
+                    this.progressBar.Increment(-1); // cancel animation for a more accurate progress bar
+                    this.progressBar.Increment(1);
+                }
+            }
+        }
+
+
+        private void StartCounting()
+        {
+            mAnsiWordCounter.Start(filenameTextBox.Text);
+
+            this.cancelButton.Enabled = true;
+            this.progressBar.Value = 0;
+            this.progressBar.Show();
+            this.progressUpdateTimer.Start();
+            ClearCountedWords();
+        }
+
+        private void CancelCounting()
+        {
+            mAnsiWordCounter.Reset();
+
+            this.cancelButton.Enabled = false;
+            this.progressBar.Hide();
+            this.progressUpdateTimer.Stop();
+            ClearCountedWords();
+        }
+
+        private void ShowCountedWords(List<CountedWord> countedWords)
+        {
+            this.dataGridView.DataSource = countedWords;
+            this.dataGridView.Columns[0].HeaderCell.Value = "Word";
+            this.dataGridView.Columns[1].HeaderCell.Value = "Occurence";
+            this.dataGridView.Show();
+        }
+
+        private void ClearCountedWords()
+        {
+            this.dataGridView.Columns.Clear();
+            this.dataGridView.DataSource = null;
+            this.dataGridView.Hide();
+        }
+    }
+}
